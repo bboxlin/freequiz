dee8d8eee0953d6eba04e8f475cd76e96aaef190
boxianglin <boxiang.lin@wsu.edu>
2023-04-10 23:48:35 -0700

    ADD NEW FEATUREgit add . Download Quiz and Upload Quiz via txt and JSON

--- js/dynamic_exporter.js
+++ js/dynamic_exporter.js
@@ -1,19 +1,42 @@
 function exportButtonListener(id) {
+    let exporter = new Map()
+    exporter.set("json", exportAsJSON)
+    exporter.set("txt", exportAsTXT)
+
     $('#' + id).click(function(event) {
         event.preventDefault();
-
-        const localStorageData = JSON.stringify(localStorage);
-        const blob = new Blob([localStorageData], {type: 'application/json'});
-
-        // Create a temporary anchor element to trigger the download
-        const downloadLink = document.createElement('a');
-        downloadLink.href = window.URL.createObjectURL(blob);
-        downloadLink.download = 'quiz.json';
-
-        // Trigger the download by appending the anchor to the document and simulating a click
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
+        let type = $("#downloadtype").val()
+        exporter.get(type)()
     })
 }
- 
+
+function exportAsJSON() {
+    let quizData = localStorage.getItem('quizData')
+    let quizArray = JSON.parse(quizData)
+    const exportedData = JSON.stringify(quizArray, null, '\t');
+    mockADownloadLink('application/json', exportedData, "downloaded_quiz.json")
+}
+
+function exportAsTXT() {
+    let quizData = localStorage.getItem('quizData')
+    let quizArray = JSON.parse(quizData)
+    let exportedData = ""
+    quizArray.forEach((item, i) => {
+        exportedData += `${i+1}. ${item.question}\n` +
+                   `${Object.entries(item.ansObj).map(([key, value]) => `${key}. ${value}`).join('\n')}\n` +
+                   `${item.correctAnsLetter}\n\n`;
+    })
+    mockADownloadLink('text/plain;charset=utf-8', exportedData, "downloaded_quiz.txt")
+}
+
+function mockADownloadLink(fileType, exportedData, outputName) {
+    const blob = new Blob([exportedData], {type: fileType});
+    const downloadLink = document.createElement('a');
+    downloadLink.href = window.URL.createObjectURL(blob);
+    downloadLink.download = outputName;
+
+    // Trigger the download by appending the anchor to the document and simulating a click
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
--- js/dynamic_importer.js
+++ js/dynamic_importer.js
@@ -1,3 +1,122 @@
 function importButtonListener(id) {
+    const importer = new Map()
+    importer.set("application/json", insertJSONtoLocalStorage)
+    importer.set("text/plain", insertTXTtoLocalStorage)
 
+    $('#' + id).click(function(event) {
+        event.preventDefault(); 
+        const fileInput = $('#fileInput');
+        const file = fileInput[0].files[0];
+        if (!file) {
+            alert('Please choose a file.');
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const importedData = e.target.result
+            const fileType = file.type
+            importer.get(fileType)(importedData)
+            fileInput.val('');
+        };
+        reader.onerror = function(e) {
+            alert('Error: Unable to read file.');
+            fileInput.val('');
+        };
+        reader.readAsText(file);
+    })
+}
+
+function insertTXTtoLocalStorage(data) {
+    const existingQuizData = localStorage.getItem('quizData');
+    const existingQuizArray = JSON.parse(existingQuizData);
+    const exitingQuestionNames = new Set();
+    const oldlen = existingQuizArray.length;
+    existingQuizArray.forEach(existingQuizItem => {
+        exitingQuestionNames.add(existingQuizItem.question);
+    });
+    quizListString = data.split("\n\n") 
+    quizListString.forEach(quizString => {
+        quizItemList = quizString.split("\n")
+        if (quizItemList.length <= 2) {
+            return // same as continue in traditional for loop
+        }
+        // quizItemList = [question, A_choice, B_choice, ....., letter of answer]
+        const id = new Date()
+        const question = parseTxtQuestion(quizItemList[0])
+        if (!exitingQuestionNames.has(question)) {
+            const ansobj = parseTxtChoices(quizItemList)
+            const ans = quizItemList[quizItemList.length - 1].trim()
+            const newQuizItem = new QuizItem(id, question, ansobj, ans)
+            existingQuizArray.push(newQuizItem)
+        }  
+        exitingQuestionNames.add(question)
+    })
+    const newlen = existingQuizArray.length;
+    // update the local sotrage
+    localStorage.setItem("quizData", JSON.stringify(existingQuizArray))
+    displayDataFromLocalStorage()
+
+    msg = `
+    ${newlen-oldlen} quiz item(s) inserted \n
+    Note: Duplicate question name will not be inserted by default!
+    `
+    alert(msg)
+}
+
+function parseTxtQuestion(s) {
+    // number. xxxxxxxxxx
+    // we want the xxxxxxxx
+    const regex = /^\d+\. +(.*)$/;
+    const match = s.match(regex);
+    return match ? match[1] : "";
+}
+
+function parseTxtChoices(arr) {
+    const ansobj = {}
+    const regex = /^([A-Za-z])\. +(.*)$/;
+    for (let i = 1; i < arr.length - 1; i++) {
+        const match = arr[i].match(regex);
+        if (match) {
+            ansobj[match[1]] = match[2];
+        }
+    }
+    return ansobj;
+}
+    
+
+function insertJSONtoLocalStorage(data) {
+    const existingQuizData = localStorage.getItem('quizData');
+    const existingQuizArray = JSON.parse(existingQuizData);
+    const exitingQuestionNames = new Set();
+    const oldlen = existingQuizArray.length;
+    existingQuizArray.forEach(existingQuizItem => {
+        exitingQuestionNames.add(existingQuizItem.question);
+    });
+
+    const newQuizArray = JSON.parse(data)
+    newQuizArray.forEach(newQuizItem => {
+        newquestion = newQuizItem.question.trim()
+        if (!exitingQuestionNames.has(newquestion)) {
+            existingQuizArray.push(newQuizItem)
+        }  
+        exitingQuestionNames.add(newquestion)
+    })
+    const newlen = existingQuizArray.length;
+    // update the local sotrage
+    localStorage.setItem("quizData", JSON.stringify(existingQuizArray))
+    displayDataFromLocalStorage()
+
+    msg = `
+    ${newlen-oldlen} quiz item(s) inserted \n
+    Note: Duplicate question name will not be inserted by default!
+    `
+    alert(msg)
+}
+
+function clearFileListener(id) {
+    $('#' + id).click(function(event) {
+        event.preventDefault(); 
+        const fileInput = $('#fileInput');
+        fileInput.val('');
+    })
 }
